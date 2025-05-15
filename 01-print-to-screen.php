55d30ca0c7b00af12ad61c744d340a1fa2072ef5
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-05-15 14:22:37 -0500

    Clase-01: Imprimir por pantalla en php.

--- 01-print-to-screen.php
+++ 01-print-to-screen.php
@@ -1,26 +1,26 @@
 <?php
     /*Imprimir por pantalla con echo.*/
 echo "Hola usando echo.<br>";
-echo "Esta es una expresión ", "Esta es otra expresión.<br>";
+echo "Este es un argumento.<br>", "Este es otro argumento.<br>";
 
     /*Imprimir por pantalla con print.*/
 print "Hola usando print";
 
 /*
         NOTAS  SOBRE (print):
-    · Es una función, por ende es más lenta q echo.
+    · (print) es una función, por ende es más lenta q (echo).
 
-    · Solo puede devolver una expresión, echo puede devolver una o varias.
+    · (print) solo puede devolver un argumento, (echo) puede devolver uno o varios argumentos a la vez.
 
-    · Devuelve el valor de uno (1) si se lo asigna a una variable, (se verá mas adelante).
+    · (print) devuelve el valor de uno (1) si se lo asigna a una variable, (se verá mas adelante).
 
-    · No utilizarlo a menos que sea estrictamete necesario.
+    · No utilizar (print) a menos que sea estrictamente necesario.
 */
 
 /*
         NOTAS SOBRE SALTO DE LÍNEA:
-    · Si se ejecuta el codigo en el navegador, hay q utilizar <br> para generar el salto de línea.
+    · Si se ejecuta el codigo en el navegador, hay q utilizar <br> para generar un salto de línea.
 
-    · Si se ejecuta en la terminal / línea de comandos / consola, hay q utilizar (\n) para generar el salto de línea.
+    · Si se ejecuta cen (la terminal / línea de comandos / consola), hay q utilizar (\n) para generar un salto de línea.
 */
 ?>

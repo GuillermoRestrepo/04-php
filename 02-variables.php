aac740622b0d97f524fd786251a5236f69e324cd
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-05-15 15:14:53 -0500

    Clase-02: variables en php, tipos de datos y cambiar el tipo de dato de una variable.

--- 02-variables.php
+++ 02-variables.php
@@ -1,4 +1,21 @@
 <?php 
+////  NOMENCLATURAS DE LAS VARIABLES EN PHP  ////
+/*
+1. Todas las variables deben comenzar con $.
+
+2. El nombre de la variable debe de empezar con una letra o con un guión bajo (_).
+
+3. Solo Pueden contener lestras, numeros y el unico caracter especial valido es el guión bajo (_).
+
+4. PHP es case sensitive (distingue entre mayusculas y minusculas), es decir ($variable;) y ($VARIABLE;), son dos variables diferentes.
+
+5. Hay dos estilos para definir variables en PHP:
+  · Camel Case: ($nombreUsuario;)  -->  Comúnmente usado
+  · Snake Case: ($nombre_usuario;)  -->  Tambien aceptado
+  · NOTA: solo usar un estilo, no mezclarlos.
+*/
+
+
 ////  DEFINIR VARIABLES  ////
 /*Para definir una variable, hay q poner el signo de dolar ($) seguido del nombre de la variable.*/
 
@@ -11,16 +28,20 @@ $apellido= "Restrepo";  // Se crea y se le da valor.
 ////  TIPOS DE DATOS  ////
 /*
     NOTA:
-· gettype(variable): sirve para saber que tipo de dato contiene una variable.
+· gettype(variable): muestra el tipo de dato de una variable.
+
+· var_dump(variable): muestra el tipo de dato y el valor de la variable, y en caso como de textos o arreglos, muestra la longitud. 
 */
 
 /*Tipo cadena (string)*/
 $cadena= "Hola";
 echo gettype($cadena), "<br>";
+echo var_dump($cadena), "<br>";
 
 /*Tipo enter (integer)*/
 $entero= 54;
 echo gettype($entero), "<br>";
+echo var_dump($entero), "<br>";
 
 /*Tipo decimal (double)*/
 $decimal= 9.1416;
@@ -51,11 +72,14 @@ echo gettype($nulo2), "<br>";
 
   · (unset) convierte a Devuelve null.
 */
-$edad= "24.5";
-echo gettype($edad), "<br>";
+$estado= true;
+echo gettype($estado), "<br>";
 
-$edad= (int) $edad;
-echo gettype($edad), "<br>";
+$estado= (int) $estado;
+echo gettype($estado), "<br>";
+
+$estado= (string) $estado;
+echo gettype($estado), "<br>";
 
 /*
 2do - Función de conversión.
@@ -67,12 +91,21 @@ echo gettype($edad), "<br>";
 
   · boolval()	boolean
 
-  · settype()	Cambia el tipo directamente
+  · settype($variabel, "tioDato"), los tipos posibles de datos son:.
+    · "string"
+    · "integer"
+    · "float"
+    · "boolean"
+    · "array"
+    · "null"
 */
 $falso= "false";
 echo gettype($falso), "<br>";
 
-$falso= boolval($falso);
+$falso= settype($falso, "boolean");
+echo gettype($falso), "<br>";
+
+$falso= intval($falso);
 echo gettype($falso), "<br>";
 
 /*

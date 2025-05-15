3223be33132cf291ad0a095224ba1056a661a14a
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-05-14 20:30:54 -0500

    Clase-01: Imprimir por pantalla en php.

---
+++ 01-print-to-screen.php
@@ -0,0 +1,26 @@
+<?php
+    /*Imprimir por pantalla con echo.*/
+echo "Hola usando echo.<br>";
+echo "Esta es una expresión ", "Esta es otra expresión.<br>";
+
+    /*Imprimir por pantalla con print.*/
+print "Hola usando print";
+
+/*
+        NOTAS  SOBRE (print):
+    · Es una función, por ende es más lenta q echo.
+
+    · Solo puede devolver una expresión, echo puede devolver una o varias.
+
+    · Devuelve el valor de uno (1) si se lo asigna a una variable, (se verá mas adelante).
+
+    · No utilizarlo a menos que sea estrictamete necesario.
+*/
+
+/*
+        NOTAS SOBRE SALTO DE LÍNEA:
+    · Si se ejecuta el codigo en el navegador, hay q utilizar <br> para generar el salto de línea.
+
+    · Si se ejecuta en la terminal / línea de comandos / consola, hay q utilizar (\n) para generar el salto de línea.
+*/
+?>

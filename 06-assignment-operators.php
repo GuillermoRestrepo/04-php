3165aec2db7de3fbaa8a83594a4ea5fee45371e2
GuillermoRestrepo <GuillermoRestrepoCode@gmail.com>
2025-05-21 14:00:27 -0500

    Clase-06: operadores de asignacíon en php.

---
+++ 06-assignment-operators.php
@@ -0,0 +1,33 @@
+<?php
+$numero;
+
+$numero= 1;
+echo "(\$numero= 1;): sirve para asignar el valor a una variable. ($numero)<br>";
+
+$numero++;
+echo "(\$numero++;): sirve para incrementar en uno el valor de una variable. ($numero)<br>";
+
+$numero+= 5;
+echo "(\$numero+= 5;): suma el número que se le indica al valor de la variable. ($numero)<br>";
+
+$numero--;
+echo "(\$numero--;): sirve para decrementar en uno el valor de una variable. ($numero)<br>";
+
+$numero-= 5;
+echo "(\$numero-= 5;): resta el número que se le indica al valor de la variable. ($numero)<br>";
+
+$numero*= 16;
+echo "(\$numero*= 16;): multiplica el número que se le indica por el valor de la variable. ($numero)<br>";
+
+$numero/= 2;
+echo "(\$numero/= 2;): divide el valor de la variable entre el número que se le indica. ($numero)<br>";
+
+$numero%= 3;
+echo "(\$numero/=3): divide el valor de la variable entre el número que se le indica y devuelve el residuo. ($numero)<br>";
+
+$numero**= 3;
+echo "(\$numero**= 3): multiplica el valor de la variable el número de veces el valor que se le indica. ($numero)<br>";
+
+echo "(+=) y (.=): también sirven para concatenar.<br>"
+
+?>
